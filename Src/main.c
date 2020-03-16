2d2d0ee7b3fe595de762ad031cd8bdd1b8e66cc6
Donald <dhaselwood@verizon.net>
2020-03-16 16:23:42 -0400

    added PC directory and canfmt

--- Src/main.c
+++ Src/main.c
@@ -312,7 +312,7 @@ DiscoveryF4 LEDs --
 
   /* Create the thread(s) */
   /* definition and creation of defaultTask */
-  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 384);
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, (384+96));
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
