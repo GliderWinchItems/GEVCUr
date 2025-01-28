026d5bf51f0db7467802f412e4d1fba00e2e7426
gsm50 <gsmoore50@gmail.com>
2025-01-27 18:58:32 -0800

    Control Law 3 PREP LED tweaks

--- Ourtasks/control_law_v3.c
+++ Ourtasks/control_law_v3.c
@@ -38,32 +38,24 @@ void control_law_v3_calc(struct DMOCCTL* pdmocctl)
         if (pdmocctl->speedact >= pdmocctl->lc.upper_speed_lmt) 
 	{ // Here at or above upper speed limit
 		direction = 0;
-		led_retrieve.mode = LED_ON;
 	}
 	else if (pdmocctl->speedact <= pdmocctl->lc.lower_speed_lmt) 
         { // Here at or below lower speed limit
 		direction = 1;
-		led_retrieve.mode = LED_OFF;
         }
-        else if (direction == 0)
-        {
-                 led_retrieve.mode = LED_ON;
-        }
-        else 
-        {
-                 led_retrieve.mode = LED_OFF
-;
-        }
+        
 
 	if (direction == 0)
 	{ 
 		/* Pct (0.01) * CL position (0-100.0) * max negative torque negative (Nm) */
 		pdmocctl->ftorquereq = 0.01f * clfunc.curpos * pdmocctl->lc.fmaxtorque_neg;
+                led_retrieve.mode = LED_ON;
 	}
 	else
 	{
 		/* Pct (0.01) * CL position (0-100.0) * max positive torque  (Nm) */
 		pdmocctl->ftorquereq = 0.01f * clfunc.curpos * pdmocctl->lc.fmaxtorque_pos;
+                led_retrieve.mode = LED_OFF;
 	}
 	xQueueSendToBack(LEDTaskQHandle,&led_retrieve,portMAX_DELAY);
 	return;
