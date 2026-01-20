31d54b1fe61e3c613af6ede437617a2d62c7e763
Donald Haselwood <dhaselwood@gmail.com>
2026-01-20 12:40:18 -0500

    update check

--- Ourtasks/mastercontroller_states.h
+++ Ourtasks/mastercontroller_states.h
@@ -1,7 +1,7 @@
 /******************************************************************************
 * File Name          : mastercontroller_states.h
 * Date First Issued  : 10/11/2020
-* Description        : Defines (super-) states for Master Controller
+* Description        : Defines states for Master Controller
 *******************************************************************************/
 
 #ifndef __MASTERCONTROLLERSTATES
@@ -24,30 +24,54 @@
 #define MC_STOP      (9 << 4)
 */
 
-/* 12/12/2025
-Payload uint16_t, i.e can.cd.us[0]
-us[0] unsigned short (little endian)
-  [15:13] Active drum number (0- 7)
-  [12: 9] Major state in effect (0 - 15)
-  [ 8: 5] Minor state (0 - 15)
-  [ 4: 0] Reserved
+/* 12/23/2025
+Payload: U8_U8_U8
+uc[0] - Major states
+  Upper 4 bits: Original states 
+  Lower 4 bits: Additional states
 
+uc[1] - sub-states
+  Upper 4 bits: Original sub-states
+  Lower 4 bits: Additional states
+
+uc[2] - drum
+  Upper 4 bits: reserved
+  Lower 3 bits: drum number 
 */
 
-/* Major states                  */
-#define MC_SAFE      (0 << 4)  //  contactor  open
-#define MC_PREP      (1 << 4)  //  contactor closed
-#define MC_ARMED     (2 << 4)  // 
-	#define MCS_TAKEUPSLACK  0 //
-	#define MCS_GRNDROLL     1 //
-#define MC_GRNDRTN   (3 << 4)  //
-#define MC_RAMP      (4 << 4)  //
-#define MC_CLIMB     (5 << 4)  //
-#define MC_RECOVERY  (6 << 4)  // 
-#define MC_RETRIEVE  (7 << 4)  //
-#define MC_ABORT     (8 << 4)  //
-#define MC_STOP      (9 << 4)  // Apply brake
+struct MC_STATE
+{
+  uint8_t major; // Major state (MC_...)
+  uint8_t sub;   // Sub-state (MCS_...)
+  uint8_t drum;  // Drum selected) (0 - 7)
+};
 
+/* Major states    12/23/2025              */
+#define MC_INIT      ( 0 << 4)  // MC Initialization
+  #define MCS_WAIT_READY          ( 0 << 4) // Wait: LCD init, CL to calibrate
+  #define MCS_WAIT_SA_SW_SAFE     ( 1 << 4) // Wait: SAFE/ACTIVE to be SAFE
 
+#define MC_SAFE      ( 1 << 4)  //  contactor  open
+  #define MCS_SAFE_INIT                 ( 0 << 4) // Open contactor
+  #define MCS_SAFE_WAIT_CONTACTOR_OPEN  ( 1 << 4) //
+
+  #define MCS_SAFE_WAIT_SA_SW_ACTIVE    ( 3 << 4) //
+  #define MCS_SAFE_WAIT_CONTACTOR_CLOSE ( 4 << 4) //
+
+#define MC_PREP      ( 2 << 4)  //  contactor closed
+  #define MCS_PREP_INIT         ( 0 << 4) //
+  #define MCS_PREP_TAKEUPSLACK  ( 1 << 4) //
+  #define MCS_PREP_GRNDROLL     ( 2 << 4) //
+
+#define MC_ARMED     ( 3 << 4)  // 
+#define MC_GRNDRTN   ( 4 << 4)  //
+#define MC_RAMP      ( 5 << 4)  //
+#define MC_CLIMB     ( 6 << 4)  //
+#define MC_RECOVERY  ( 7 << 4)  // 
+#define MC_RETRIEVE  ( 8 << 4)  //
+#define MC_ABORT     ( 9 << 4)  //
+#define MC_STOP      (10 << 4)  // Apply brake
 
 #endif
+
+
